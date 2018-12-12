7f9d4b46dd4af45f99f37ba726d2b04eaa67ffa9
Jakub Bieńkowski <jbienkowski311@gmail.com>
2018-12-12 21:22:24 +0100

    Do not calculate value in seconds every time

--- 302/Date.cpp
+++ 302/Date.cpp
@@ -1,8 +1,8 @@
 #include "Date.hpp"
 
-Date::Date() : D(0), M(0), Y(0), h(0), m(0), s(0) {}
+Date::Date() : D(0), M(0), Y(0), h(0), m(0), s(0), inSeconds(0) {}
 Date::Date(int D, int M, int Y, int h, int m, int s)
-    : D(D), M(M), Y(Y), h(h), m(m), s(s) {}
+    : D(D), M(M), Y(Y), h(h), m(m), s(s), inSeconds(toSeconds()) {}
 
 Date &Date::operator=(const Date &d) {
   D = d.D;
@@ -11,38 +11,29 @@ Date &Date::operator=(const Date &d) {
   h = d.h;
   m = d.m;
   s = d.s;
+  inSeconds = d.inSeconds;
   return *this;
 }
 
-bool Date::operator<(const Date &d) { return toSeconds() < d.toSeconds(); }
+bool Date::operator<(const Date &d) { return inSeconds < d.inSeconds; }
 
-bool Date::operator<(const Date &d) const {
-  return toSeconds() < d.toSeconds();
-}
+bool Date::operator<(const Date &d) const { return inSeconds < d.inSeconds; }
 
-bool Date::operator<=(const Date &d) { return toSeconds() <= d.toSeconds(); }
+bool Date::operator<=(const Date &d) { return inSeconds <= d.inSeconds; }
 
-bool Date::operator<=(const Date &d) const {
-  return toSeconds() <= d.toSeconds();
-}
+bool Date::operator<=(const Date &d) const { return inSeconds <= d.inSeconds; }
 
-bool Date::operator>(const Date &d) { return toSeconds() > d.toSeconds(); }
+bool Date::operator>(const Date &d) { return inSeconds > d.inSeconds; }
 
-bool Date::operator>(const Date &d) const {
-  return toSeconds() > d.toSeconds();
-}
+bool Date::operator>(const Date &d) const { return inSeconds > d.inSeconds; }
 
-bool Date::operator>=(const Date &d) { return toSeconds() >= d.toSeconds(); }
+bool Date::operator>=(const Date &d) { return inSeconds >= d.inSeconds; }
 
-bool Date::operator>=(const Date &d) const {
-  return toSeconds() >= d.toSeconds();
-}
+bool Date::operator>=(const Date &d) const { return inSeconds >= d.inSeconds; }
 
-bool Date::operator==(const Date &d) { return toSeconds() == d.toSeconds(); }
+bool Date::operator==(const Date &d) { return inSeconds == d.inSeconds; }
 
-bool Date::operator==(const Date &d) const {
-  return toSeconds() == d.toSeconds();
-}
+bool Date::operator==(const Date &d) const { return inSeconds == d.inSeconds; }
 
 long long int Date::toSeconds() const {
   return s + 60 * (m + 60 * (h + 24 * (D + 31 * ((long long)M + 12 * Y))));
